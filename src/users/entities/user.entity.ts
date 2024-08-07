f2b44b8307c4d92af1e1cce8f92ed6246d9354e0
Redgorila <agmintroap@gmail.com>
2024-08-07 08:06:15 +0200

    Mapping user entity to database table

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,26 @@
+import { RegistryDates } from './../../common/embedded/registry-dates.embedded';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+  @Column()
   email: string;
+  @Column({ unique: true })
   phone: string;
+  @Column({ unique: true })
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
